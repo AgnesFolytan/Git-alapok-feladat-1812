eb87f79655b12fa6341a3918235ce12b5d077838
Petrik Diak <diak@petrik.hu>
2023-12-18 12:40:08 +0100

    3. feladat kész

--- 1812/Program.cs
+++ 1812/Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Adjon meg egy számot: ");
 int szam = 1;
+List<int> lista = new List<int>();
 while (szam != 0)
 {
-	szam = int.Parse(Console.ReadLine());
+    foreach (var item in lista)
+    {
+        Console.Write(item + ", ");
+    }
+    szam = int.Parse(Console.ReadLine());
+	lista.Add(szam);
+
 }
